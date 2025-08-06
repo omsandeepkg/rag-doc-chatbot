2c52c64ffc3380f2b4a196672f4c41ce67aad6f8
omsandeepkg <98887049+omsandeepkg@users.noreply.github.com>
2025-08-07 04:22:43 +0530

    Update document_loader.py

--- utils/document_loader.py
+++ utils/document_loader.py
@@ -2,7 +2,15 @@ from langchain.document_loaders import PyMuPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
 
 def load_and_chunk(uploaded_file):
-    loader = PyMuPDFLoader(uploaded_file)
+     # Save the uploaded file to a temporary file
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
+        tmp_file.write(uploaded_file.read())
+        tmp_file_path = tmp_file.name
+
+     # Now pass the path to PyMuPDFLoader
+    loader = PyMuPDFLoader(tmp_file_path)
     documents = loader.load()
+    #loader = PyMuPDFLoader(uploaded_file)
+    #documents = loader.load()
     splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     return splitter.split_documents(documents)
